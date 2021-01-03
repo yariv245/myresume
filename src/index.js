eb605e400b1cf77b72a318fa161838596edf0e70
Yariv Menachem <yariv220895@gmail.com>
2021-01-03 14:05:06 +0200

    installing more moudlus for resume

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import Landing from './Components/landingPage/Landing';
 import About from './Components/About/About';
 import Header from './Components/Header';
+// import jQuery from 'jquery';
+import $ from 'jquery';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'jquery/dist/jquery.min.js';
 import 'bootstrap/dist/js/bootstrap.min.js';
@@ -10,13 +12,14 @@ import './css/animate.css';
 import './css/animation.css';
 import './css/style.css';
 import './css/font-awesome.css';
-import $ from 'jquery';
-
-// import './js/jquery-scrolltofixed.js';
-// import './js/jquery.nav.js';
+import 'jquery-easing';
+// import 'jquery.fancybox/source/jquery.fancybox.pack.js';
 // import './js/jquery.easing.1.3.js';
-// import './js/jquery.isotope.js';
+// import './js/jquery-scrolltofixed.js';    ???
 // import './js/fancybox/jquery.fancybox.pack.js';
+
+// import './js/jquery.nav.js';
+// import './js/jquery.isotope.js';
 // import './js/wow.js';
 // import './contact/jqBootstrapValidation.js';
 // import './contact/contact_me.js';
